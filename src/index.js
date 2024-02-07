59460ed962ca14f132fe96aab0de471b746c0141
Yuri <+>
2024-02-07 08:59:01 -0500

    My ramen Rater

--- src/index.js
+++ src/index.js
@@ -1,21 +1,52 @@
 // index.js
 
 // Callbacks
-const handleClick = (ramen) => {
-  // Add code
+const handleClick = () => {
+   const img = document.createElement('img')
+  img.src = ramens.image
+  ramenMenu.appendChild(img)
+  img.addEventListener('click', () => {
+    displayRamens(ramens.target.ramen-menu.alt)
+  })
 };
 
 const addSubmitListener = () => {
-  // Add code
+  document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('form')
+    form.document.addEventListener('submit',(event) => {
+    event.preventDefault()
+    buildRamen(event.target.ramen-menu.alt)
+    })
+ })
 }
 
-const displayRamens = () => {
-  // Add code
-};
+ 
+
+const displayRamens = (ramens) => {
+  fetch('http://localhost:3000/ramens/:id')
+  .then(response => response.json())
+  .then(ramens => {
+
+   
+    ramens.forEach((ramens) => {
+      displayRamens(ramens)
+      const newRamen = document.getElementById('ramen-menu')
+      newRamen
+    })
+  })
+  const detailedImages = document.querySelector('detail-image')
+  detailedImages.class = ramens[0].images
+  const nameElement = document.querySelector('name')
+  nameElement.textContent = ramens[0].name
+  const displayElement = document.getElementsByClassName("restaurant")
+  displayElement.textContent = ramens[0].restaurant
+}
 
 const main = () => {
   // Invoke displayRamens here
+  displayRamens(ramens)
   // Invoke addSubmitListener here
+  addSubmitListener()
 }
 
 main()
