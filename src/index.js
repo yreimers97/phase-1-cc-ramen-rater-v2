fddd957749662d8d898fe63ebce2b7da68b2f825
Yuri <+>
2024-02-16 08:14:17 -0500

    My Ramen

--- src/index.js
+++ src/index.js
@@ -1,53 +1,63 @@
 // index.js
-
+  
 // Callbacks
 const handleClick = (food) => {
-  //const imageElement = document.getElementById('image')
-    //imageElement.src = food.image
-    //const nameElement = document.getElementById('name')
-    //nameElement.textContent = food.name
-    const ramenDetails = document.getElementById('ramen-details')
-    const ramenText = document.createElement('alt')
-
-    //ramenDetails.textContent = food.
-  
-  // const foodDetails = document.getElementById('ramen-details')
-  // foodDetails.img = food.image
-  // const ramenName = document.createElement('name')
-  // ramenName.textContent = food.name
- 
-   
-// })
+  //const ramenDetail = document.getElementById('ramen-detail')
+  const myMenu = document.querySelector(".detail-image")
+  myMenu.src = food.image
+  const ramenDetail = document.querySelector('.name')
+  ramenDetail.innerText = food.name
+  const foodName = document.querySelector('.restaurant')
+  foodName.textContent = food.restaurant
+  const rateRamen = document.getElementById('rating-display')
+  rateRamen.textContent = food.rating
+  const commentRamen = document.getElementById('comment-display')
+  commentRamen.innerText = food.comment
 }
 
 const addSubmitListener = () => {
-  // const newRamen = document.getElementById('new-ramen')
-  //   newRamen.addEventListener('submit', (event) => {
-  //       event.preventDefault()
-       
-}
+  const addForm = document.getElementById('new-ramen');
+  addForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const newName = document.createElement('new-name').value
+    const newRestaurant = document.getElementById('new-restaurant').value
+    const newImage = document.getElementById('new-image').value
+    const newRating = document.getElementById('new-rating').value
+    const newComment = document.getElementById('new-comment').value
 
-const displayRamens = () => {
-  
-  fetch('http://localhost:3000/ramens')
-  .then(response => response.json())
-  .then(ramens => {
-    ramens.forEach(food => {
-      const myRestaurant = document.getElementById("ramen-menu")
-      const imgElement = document.createElement('img')
-      imgElement.src = food.image
-      myRestaurant.appendChild(imgElement)
-
-      imgElement.addEventListener('click', () => {
-        handleClick(food)
-      })
-    })
+    let newRamen = {
+      name: newName,
+      restaurant: newRestaurant,
+      image: newImage,
+      rating: newRating,
+      comment: newComment
+    }
+    console.log(newRamen)
   })
+}
+fetch('http://localhost:3000/ramens')
+.then(response => response.json())
+.then(ramens => displayRamens(ramens))
+
+const displayRamens = (ramens) => {
+  // fetch('http://localhost:3000/ramens')
+  // .then(response => response.json())
+  // .then(ramens => food{ })
+  // //displayRamens()
+
+  ramens.forEach(food => {
+    const imgElement = document.createElement('img')
+    imgElement.src = food.image
+    const myRestaurant = document.getElementById("ramen-menu")
+    myRestaurant.appendChild(imgElement)
+
+      imgElement.addEventListener('click', () => handleClick(food))
+  })     
 }
 
 const main = () => {
   // Invoke displayRamens here
-  displayRamens()
+  //displayRamens((newRamen))
   // Invoke addSubmitListener here
   addSubmitListener()
 }
